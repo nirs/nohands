2b62faf4dcafc2016403e1300457edcea2c4b52f
Nir Soffer <nsoffer@redhat.com>
2020-06-30 00:01:54 +0300

    Add GREY color

--- demo.py
+++ demo.py
@@ -4,8 +4,9 @@
 import subprocess
 import time
 
-__all__ = ["msg", "run", "GREEN", "CYAN", "YELLOW"]
+__all__ = ["msg", "run", "GREEN", "CYAN", "YELLOW", "GREY"]
 
+GREY = "\033[38;5;250m"
 GREEN = "\033[1;32m"
 CYAN = "\033[1;36m"
 YELLOW = "\033[1;33m"
