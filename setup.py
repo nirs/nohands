a83e64cecd4999824bac50f11c5e44ec0e3a5a34
Nir Soffer <nsoffer@redhat.com>
2022-01-04 21:37:50 +0200

    Packaging
    Add setup.py and MANIFEST.in.

---
+++ setup.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: Nir Soffer <nirsof@gmail.com
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup
+
+import nohands
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    author="Nir Soffer",
+    author_email="nirsof@gmail.com",
+    description="A little library for effortless demos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    name="nohands",
+    url="https://github.com/nirs/nohands",
+    packages=["nohands"],
+    project_urls={
+        "Source": "https://github.com/nirs/nohands",
+        "Bug Tracker": "https://github.com/nirs/nohands/issues",
+    },
+    version=nohands.__version__,
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+    ],
+)
